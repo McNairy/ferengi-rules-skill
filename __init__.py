60281fa7e14a7a6eaa2e613d21825de1aa58cebe
Michael McNairy <michaelmcnairy@hotmail.com>
2020-10-14 12:56:50 -0400

    inexplicably not working

--- __init__.py
+++ __init__.py
@@ -9,7 +9,11 @@ class FerengiRules(MycroftSkill):
     def handle_rules_ferengi(self, message):
         spoken_number = message.data.get('number')
         number = extract_number(spoken_number, ordinals=True)
-        rules = self.translate_list('rules_of_acquisition')
+        rules = self.translate_list('rules_of_acquisition.list')
+        #translated = self.translate_namedvalues('rules_of_acquisition')
+        #self.log.info("DEBUG BEGIN")
+        #self.log.info(translated[number])
+        #self.log.info("DEBUG END")
         self.speak_dialog(rules[number - 1])
 
 def create_skill():
