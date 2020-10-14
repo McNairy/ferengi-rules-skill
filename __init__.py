8321151913cdf630ec4b01ebb7f905b1e14a008a
Michael McNairy <michaelmcnairy@hotmail.com>
2020-10-14 08:35:07 -0400

    it works!

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util import extract_number
 
 
 class FerengiRules(MycroftSkill):
@@ -7,8 +8,14 @@ class FerengiRules(MycroftSkill):
 
     @intent_file_handler('rules.ferengi.intent')
     def handle_rules_ferengi(self, message):
-        self.speak_dialog('rules.ferengi')
-
+        spoken_number = message.data.get('number')
+        number = extract_number(spoken_number, ordinals=True)
+        self.log.info("DEBUG: number")
+        self.log.info(number)
+        rules = self.translate_list('rules_of_acquisition')
+        self.log.info("DEBUG: rule")
+        self.log.info(rules[number -1])
+        self.speak_dialog(rules[number - 1])
 
 def create_skill():
     return FerengiRules()
